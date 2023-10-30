dbe579f5e35bd306a6cb03964c356f95f374ebf2
IndrajitParmar <parindra231@gmail.com>
2023-10-30 20:47:02 +0530

    add image uploading functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import { app } from "../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [upload, setUpload] = useState(false);
+  console.log(formData);
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleImageSubmit = (e) => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUpload(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUpload(false);
+        })
+        .catch((error) => {
+          setImageUploadError("Image upload failed (2 mb max per image)");
+          setUpload(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUpload(false);
+    }
+    // setUpload(false);
+  };
+
+  const handleDelteImage = (i) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, index) => index !== i),
+    });
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -114,11 +186,33 @@ function CreateListing() {
               accept="image/*"
               multiple
               className="p-3 border border-gray-400 rounded w-full cursor-pointer"
+              onChange={(e) => setFiles(e.target.files)}
             />
-            <span className="p-3 border border-green-700 rounded text-green-700 text-center uppercase  cursor-pointer hover:shadow-lg disabled:opacity-80">
-              upload
-            </span>
+            <button
+              type="button"
+              disabled={upload}
+              onClick={handleImageSubmit}
+              className="p-3 border border-green-700 rounded text-green-700 text-center uppercase  cursor-pointer hover:shadow-lg disabled:opacity-80">
+              {upload ? "uploading..." : "upload"}
+            </button>
           </div>
+          <p className="text-red-700 text-sm">
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, i) => (
+              <div
+                key={i}
+                className="flex justify-between p-3 border items-center">
+                <img src={url} alt="image" className="w-20 h-20 object-cover" />
+                <button
+                  type="button"
+                  onClick={() => handleDelteImage(i)}
+                  className="uppercase text-red-700 p-3 rounded-lg hover:opacity-75">
+                  delete
+                </button>
+              </div>
+            ))}
           <button className="p-3 w-full bg-slate-700 text-white uppercase rounded-lg hover:opacity-95 disabled:opacity-80">
             create listing
           </button>
