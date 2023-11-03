0ff8ae4ec37f4c5c134ada0bc5b0186c6531946c
IndrajitParmar <parindra231@gmail.com>
2023-11-03 19:51:59 +0530

    commplete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,45 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sky Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Sky Estate Agency, your premier destination for all your real
+        estate needs. Whether you're looking to sell, buy, or rent a property,
+        we provide a seamless and user-friendly platform that connects property
+        owners with potential buyers or renters. With a diverse portfolio of
+        listings ranging from cozy apartments to sprawling estates, we cater to
+        a wide array of preferences and budgets. Our team of dedicated
+        professionals is committed to ensuring a smooth and hassle-free
+        experience, making Sky Estate Agency the go-to marketplace for all
+        things real estate.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Selling or renting your property has never been easier than with Sky
+        Estate Agency. Our intuitive platform empowers property owners with the
+        tools they need to list their properties, complete with high-quality
+        photos and detailed descriptions. We offer a range of customizable
+        options to help you showcase your property in the best possible light,
+        ensuring it stands out in the competitive market. Our advanced search
+        filters and personalized recommendations make it effortless for
+        potential buyers or renters to find their dream property. With Sky
+        Estate Agency, you have the power to take control of your real estate
+        journey.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Sky Estate Agency, we prioritize transparency, security, and
+        efficiency. Our user-friendly interface ensures a seamless experience
+        for both buyers and sellers, while our robust verification process helps
+        protect against fraudulent listings. Our team of experienced agents is
+        always on hand to provide expert guidance and support throughout the
+        entire process. Whether you're a first-time buyer, a seasoned investor,
+        or a property owner looking to list, Sky Estate Agency is here to
+        elevate your real estate experience. Join us today and discover the
+        endless possibilities that await in the world of real estate.
+      </p>
+    </div>
+  );
 }
